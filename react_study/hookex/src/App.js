33eef7c2777d429e81ec42f87b335bccdeeab5bb
GeonWoo <69147082+Y-gw@users.noreply.github.com>
2023-09-12 15:14:50 +0900

    webapp week-2 react study, Use State

---
+++ react_study/hookex/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import FavoriteColor from './FavoriteColor';
+import Car from './Car';
+import Counter from './Counter';
+import UseRefPrev from './UseRefPrev';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+      <Car />
+    </div>
+  );
+}
+
+export default App;
---
+++ react_study/hookex/src/Calculator.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function Calculator(props) {
+    const [xvalue, setXvalue] = useState(0);
+    const [yvalue, setYvalue] = useState(0);
+    const [result, setResult] = useState(0);
+
+    const add = () => {
+        console.log(xvalue);
+        console.log(yvalue);
+        setResult(parseInt(xvalue) + parseInt(yvalue));
+    }
+
+    const sub = () => {
+        console.log(xvalue);
+        console.log(yvalue);
+        setResult(parseInt(xvalue) - parseInt(yvalue));
+    }
+
+    const mul = () => {
+        console.log(xvalue);
+        console.log(yvalue);
+        setResult(parseInt(xvalue) * parseInt(yvalue));
+    }
+
+    const div = () => {
+        console.log(xvalue);
+        console.log(yvalue);
+        setResult(parseInt(xvalue) / parseInt(yvalue));
+    }
+    
+    return ( 
+        <>
+            <h1>My Calculator</h1>
+            <input name='xvalue' onChange={(e)=>{setXvalue(e.target.value)}}/>
+            <br/>
+            <input name='yvalue' onChange={(e)=>{setYvalue(e.target.value)}}/>
+            <br/>
+            RESULT: <span id="result">{result}</span><br/><br/>
+            <button value="+" onClick={add}>SUM(+)</button>
+            <button value="-" onClick={sub}>SUB(-)</button>
+            <button value="*" onClick={mul}>MUL(x)</button>
+            <button value="/" onClick={div}>DIV(/)</button>
+        </> 
+    );
+}
+
+export default Calculator;
